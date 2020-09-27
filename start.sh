9392fee9ce77bba010545cdddfaa40dc188c88fa
defracted <51153795+defracted@users.noreply.github.com>
2020-09-27 14:42:59 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,7 +21,7 @@ fi
 
 AIKAR_FLAGS="-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
 
-printf "\n{GREEN}Your Minecraft server is starting\n{YELLOW}Use {NC}screen -R minecraft {YELLOW} to open the console.\n\n";
+printf "\n${GREEN}Your Minecraft server is starting\n${YELLOW}Use ${NC}screen -R minecraft ${YELLOW} to open the console.\n\n";
 
 screen -d -m -S minecraft java -Xmx${XMX_VALUE} ${AIKAR_FLAGS} -jar ${CORE} nogui
 

45e38928ebe22d3cada6fac2a52bf9de6b0192a2
defracted <51153795+defracted@users.noreply.github.com>
2020-09-27 14:34:11 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+cd "$(dirname "$0")"
 
-screen -d -m -S minecraft java -Xms2G -Xmx3G -XX:+UseConcMarkSweepGC -jar paper.jar
+YELLOW="\033[1;33m"
+RED="\033[1;31m"
+NC="\033[0m"
+
+if [ "$#" -eq "0" ]; then
+	printf "${RED}[ERROR] ${YELLOW}You have to pass a value for -Xmx flag\n"
+	exit 1
+fi
+
+XMX_VALUE=$1
+CORE="paper.jar"
+
+if [ ! -f "$CORE" ]; then
+	printf "${RED}[ERROR] ${YELLOW}${CORE} was not found in current directory\n"
+	exit 1
+fi
+
+AIKAR_FLAGS="-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
+
+screen -d -m -S minecraft java -Xmx${XMX_VALUE} ${AIKAR_FLAGS} -jar ${CORE} nogui
+
